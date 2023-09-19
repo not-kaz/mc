7824da57a57b073a2a325e4f19765384e04242cf
not-kaz <kazislav@protonmail.com>
2023-09-19 21:15:50 +0200

    Renamed shader_create() to shader_build() to prevent confusion w/ heap alloc funcs.

--- include/shader.h
+++ include/shader.h
@@ -1,7 +1,7 @@
 #ifndef SHADER_H
 #define SHADER_H
 
-unsigned int shader_create(const char *vsrc, const char *fsrc);
+unsigned int shader_build(const char *vsrc, const char *fsrc);
 void shader_use(unsigned int sh);
 void shader_set_mat4f(unsigned int sh, const char *name, const float *val);
 
