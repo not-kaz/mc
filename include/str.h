a6d97261540561fc569efb5053e4b28322d4ac13
not-kaz <kazislav@protonmail.com>
2023-09-19 13:52:23 +0200

    Placed string related funcs into own source/header.

---
+++ include/str.h
@@ -0,0 +1,8 @@
+#ifndef STR_H
+#define STR_H
+
+char *str_duplicate(char *str);
+char *str_split(char **str_p, const char *delim);
+char *str_read_file(char *filename);
+
+#endif
---
+++ src/str.c
@@ -0,0 +1,63 @@
+#include "str.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+char *str_duplicate(char *str)
+{
+	size_t len;
+	void *new;
+
+	len = strlen(str) + 1;
+	new = malloc(len);
+	if (!new) {
+		return NULL;
+	}
+	return memcpy(new, str, len);
+}
+
+char *str_split(char **str_p, const char *delim)
+{
+	char *start, *end;
+
+	start = *str_p;
+	if (!start) {
+		return NULL;
+	}
+	end = start + strcspn(start, delim);
+	if (*end) {
+		*end++ = '\0';
+		*str_p = end;
+	} else {
+		*str_p = NULL;
+	}
+	return start;
+}
+
+char *str_read_file(const char *filename)
+{
+	FILE *fp;
+	char *src, buf;
+	size_t len;
+
+	fp = fopen(filename, "rb+");
+	if (!fp) {
+		return NULL;
+	}
+	fseek(fp, 0, SEEK_END);
+	len = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	src = calloc(len + 1, sizeof(char));
+	if (!src) {
+		fclose(fp);
+		return NULL;
+	}
+	len = 0;
+	while ((buf = (char)(fgetc(fp))) != EOF) {
+		src[len] = buf;
+		src[len + 1] = '\0';
+		++len;
+	}
+	fclose(fp);
+	return src;
+}
