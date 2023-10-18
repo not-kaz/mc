81885046061e7d2e36b1d3a6b02fede8ce44ebcd
not-kaz <kazislav@protonmail.com>
2023-10-18 14:32:30 +0200

    Restructured keyboard input. Keyboard state no longer exposed.

--- include/input.h
+++ include/input.h
@@ -3,6 +3,17 @@
 
 #include "types.h"
 
-void input_poll_events(const uint8_t **keystates);
+enum input_keybind_tag {
+	KEYBIND_FORWARD = 0,
+	KEYBIND_BACKWARD,
+	KEYBIND_LEFT,
+	KEYBIND_RIGHT,
+	KEYBIND_JUMP,
+	KEYBIND_CROUCH,
+	NUM_KEYBINDS
+};
+
+void input_poll_events(void);
+bool input_is_keybind_pressed(enum input_keybind_tag keybind);
 
 #endif
--- src/input.c
+++ src/input.c
@@ -4,14 +4,37 @@
 
 #include <SDL2/SDL.h>
 
-void input_poll_events(const uint8_t **keystates)
+struct keybind {
+	enum input_keybind_tag bind;
+	int scancode;
+};
+
+/* Keybinds need to be sorted the same as the keybind enums in the header. */
+static struct keybind keybinds[NUM_KEYBINDS] = {
+	{KEYBIND_FORWARD, SDL_SCANCODE_W},
+	{KEYBIND_BACKWARD, SDL_SCANCODE_S},
+	{KEYBIND_LEFT, SDL_SCANCODE_A},
+	{KEYBIND_RIGHT, SDL_SCANCODE_D},
+	{KEYBIND_JUMP, SDL_SCANCODE_SPACE},
+	{KEYBIND_CROUCH, SDL_SCANCODE_LCTRL}
+};
+
+static const uint8_t *keyboard_state;
+
+void input_poll_events(void)
 {
 	SDL_Event event;
 
-	(*keystates) = SDL_GetKeyboardState(NULL);
+	keyboard_state = SDL_GetKeyboardState(NULL);
 	while (SDL_PollEvent(&event)) {
 		if (event.type == SDL_QUIT) {
 			DIE("Quit even called. Shutting down...");
 		}
 	}
+}
+
+// HACKME: Use a ternary if statement to return true/false bool instead of u8.
+bool input_is_keybind_pressed(enum input_keybind_tag keybind)
+{
+	return keyboard_state[keybinds[keybind].scancode] ? true : false;
 }
