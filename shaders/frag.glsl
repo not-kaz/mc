53c4ac3bf0484cc9f33b3bd8b4cdd518d937f272
not-kaz <kazislav@protonmail.com>
2023-10-28 23:00:05 +0200

    Reworked fragment shader to handle sprites from atlas.

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -5,7 +5,14 @@ in vec2 tex_coord;
 
 uniform sampler2D tex_1;
 
+uniform vec2 sprite_idx;
+uniform vec2 sprite_px_size;
+
 void main()
 {
-    frag_color = texture(tex_1, tex_coord);
+    vec2 position = vec2(sprite_idx.x/16.0, sprite_idx.y/16.0);
+    vec2 tex_size =  vec2(1.0/sprite_px_size.x, 1.0/sprite_px_size.y);
+
+    vec2 coord = position + tex_size * fract(tex_coord);
+    frag_color = texture(tex_1, coord);
 }
