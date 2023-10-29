980473d8a166b851481d85af18ee334d05b6425e
not-kaz <kazislav@protonmail.com>
2023-10-29 14:02:16 +0100

    Simplified frag shader variable names.

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -5,14 +5,13 @@ in vec2 tex_coord;
 
 uniform sampler2D tex_1;
 
-uniform vec2 sprite_idx;
-uniform vec2 sprite_px_size;
+uniform vec2 offset;
+uniform vec2 size;
 
 void main()
 {
-    vec2 position = vec2(sprite_idx.x/16.0, sprite_idx.y/16.0);
-    vec2 tex_size =  vec2(1.0/sprite_px_size.x, 1.0/sprite_px_size.y);
-
+    vec2 position = vec2(offset.x/16.0, offset.y/16.0);
+    vec2 tex_size =  vec2(1.0/size.x, 1.0/size.y);
     vec2 coord = position + tex_size * fract(tex_coord);
     frag_color = texture(tex_1, coord);
 }
