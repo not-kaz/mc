d2a9c3ab3868bd8d6d2f14691c683397693a2a3b
not-kaz <kazislav@protonmail.com>
2023-12-01 14:14:15 +0100

    Added types and camera_get_position() func.

--- include/camera.h
+++ include/camera.h
@@ -3,9 +3,12 @@
 
 #include "types.h"
 
+#include <cglm/types.h>
+
 struct camera *camera_create(void);
 void camera_destroy(struct camera *cam);
 void camera_update(struct camera *cam);
 void camera_calc_view_matrix(struct camera *cam, float view_out[4][4]);
+void camera_get_position(struct camera *cam, vec2 pos_out);
 
 #endif
