eddd76b65cf221c63feeee3731a35a66e7355d18
not-kaz <kazislav@protonmail.com>
2023-12-07 12:20:47 +0100

    Refactored shader directory and naming scheme.

---
+++ shaders/block_textured_vert.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+
+layout (location = 0) in vec3 pos_attr;
+layout (location = 1) in vec2 tex_coord_attr;
+
+out vec2 tex_coord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec3 block_pos;
+
+void main()
+{ 
+    gl_Position = projection * view * model * vec4(pos_attr + block_pos, 1);
+    tex_coord = vec2(tex_coord_attr.x, tex_coord_attr.y);
+}
