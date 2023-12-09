2bf36fea662d0d5a0a4cdfeed31583127cae6380
not-kaz <kazislav@protonmail.com>
2023-12-09 12:59:09 +0100

    Removed unused macro. Added assert macro alias.

--- include/common.h
+++ include/common.h
@@ -1,8 +1,10 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <assert.h>
+
+#define ASSERT(expr) assert((expr))
 #define UNUSED(x) ((void)(x))
 #define ARRAY_SIZE(arr) (sizeof((arr)) / sizeof((arr)[0]))
-#define FOR_RANGE(x, rng) for (int (x) = (-rng); (x) <= (rng); (x++))
 
 #endif
