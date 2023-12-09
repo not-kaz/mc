a630b071fc7bd8c648ecc27130ccd8fe838eaec3
not-kaz <kazislav@protonmail.com>
2023-12-09 17:02:09 +0100

    Added some additional cube information.

--- include/cube.h
+++ include/cube.h
@@ -1,38 +1,52 @@
 #ifndef CUBE_H
 #define CUBE_H
 
+#define CUBE_NUM_COMPS_PER_VERT 5
+#define CUBE_NUM_VERTS_PER_FACE 4
+#define CUBE_NUM_COMPS_PER_FACE (CUBE_NUM_COMPS_PER_VERT * CUBE_NUM_VERTS_PER_FACE)
+
+enum cube_face {
+	CUBE_FACE_FRONT = 0,
+	CUBE_FACE_BACK,
+	CUBE_FACE_LEFT,
+	CUBE_FACE_RIGHT,
+	CUBE_FACE_TOP,
+	CUBE_FACE_BOTTOM,
+	NUM_CUBE_FACES
+};
+
 // TODO: Fix these incorrect vertices, not correctly assigned.
 float cube_vertices[] = {
 	// front
 	-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-	0.5f, -0.5f, 0.5f, 0.5f, 0.0f,
-	0.5f, 0.5f, 0.5f, 0.5f, -0.5f,
-	-0.5f, 0.5f, 0.5f, 0.0f, -0.5f,
+	0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+	0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+	-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
 	// back
 	0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-	0.5f, 0.5f, -0.5f, 0.5f, 0.0f,
-	-0.5f, 0.5f, -0.5f, 0.5f, -0.5f,
-	-0.5f, -0.5f, -0.5f, 0.0f, -0.5f,
+	0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+	-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
 	//left
 	-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-	-0.5f, 0.5f, -0.5f,0.0f, 0.0f,
-	-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-	-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+	-0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+	-0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+	-0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
 	//right
 	0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-	0.5f, -0.5f, -0.5f, 0.5f, 0.0f,
-	0.5f, 0.5f, -0.5f, 0.5f, -0.5f,
-	0.5f, 0.5f, 0.5f, 0.0f, -0.5f,
+	0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+	0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+	0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
 	//top
 	-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-	0.5f, 0.5f, 0.5f, 0.5f, 0.0f,
-	0.5f, 0.5f, -0.5f, 0.5f, -0.5f,
-	-0.5f, 0.5f, -0.5f, 0.0f, -0.5f,
+	0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+	0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+	-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
 	//bottom
 	0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-	0.5f, -0.5f, -0.5f, 0.5f, 0.0f,
-	-0.5f, -0.5f, -0.5f, 0.5f, -0.5f,
-	-0.5f, -0.5f, 0.5f, 0.0f, -0.5f
+	0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+	-0.5f, -0.5f, 0.5f, 0.0f, 1.0f
 };
 
 unsigned int cube_elements[] = {
