3faff5901689687e5b47ed5439c2da862107e49b
not-kaz <kazislav@protonmail.com>
2023-12-17 16:04:22 +0100

    Simplified block code.

--- include/block.h
+++ include/block.h
@@ -1,10 +1,6 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
-#include "types.h"
-
-#define BLOCK_SIZE 1 // Size of a block in world unit terms.
-
 enum block_type {
 	BLOCK_TYPE_AIR = 0,
 	BLOCK_TYPE_DIRT,
@@ -13,29 +9,25 @@ enum block_type {
 	NUM_BLOCK_TYPES
 };
 
-enum block_face {
-	BLOCK_FACE_NONE = 0,
-	BLOCK_FACE_FRONT = 1 << 0,  // 1
-	BLOCK_FACE_BACK = 1 << 1,  // 2
-	BLOCK_FACE_LEFT = 1 << 2,  // 4
-	BLOCK_FACE_RIGHT = 1 << 3,  // 8
-	BLOCK_FACE_TOP = 1 << 4,  // 16
-	BLOCK_FACE_BOTTOM = 1 << 5,  // 32
-	BLOCK_FACE_ALL = (1 << 6) - 1,
-	NUM_BLOCK_FACES = 6
+enum block_side {
+	BLOCK_SIDE_FRONT = 0,
+	BLOCK_SIDE_BACK,
+	BLOCK_SIDE_LEFT,
+	BLOCK_SIDE_RIGHT,
+	BLOCK_SIDE_TOP,
+	BLOCK_SIDE_BOTTOM,
+	NUM_BLOCK_SIDE
 };
 
 struct block {
 	int x;
 	int y;
 	int z;
-	flag_t visible_faces;
 	enum block_type type;
 };
 
 void block_init(struct block *block, int x, int y, int z, enum block_type type);
-void block_build_shared_mesh(void);
-void block_draw(struct block *block, unsigned int shd);
-void block_set_visible_face(struct block *block, int flag);
+void block_get_sprite_offset(enum block_type type, enum block_side side,
+	int *off_x, int *off_y);
 
 #endif
--- src/block.c
+++ src/block.c
@@ -8,54 +8,26 @@
 
 #include <cglm/types.h>
 
-#define DEFAULT_TEXTURE_SIZE_PX 16.0f
+#define NUM_BLOCK_SIDES 6
 
 struct block_texture_data {
 	enum block_type block_type;
 	struct {
 		int offset_x;
 		int offset_y;
-	} face[6];
+	} side[NUM_BLOCK_SIDES];
 };
 
-// TODO: All this needs to be heavily simplified.
-// Blocks should only contain bare minimum info / data.
-
-// TODO: Make this customizable, read these values from a json file.
-// Consider a function that returns these values for simplicity.
 struct block_texture_data texture_data[] = {
 	/*     TYPE         FRONT   BACK    LEFT    RIGHT   TOP    BOTTOM */
+	{BLOCK_TYPE_AIR, {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}},
 	{BLOCK_TYPE_DIRT, {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}},
 	{BLOCK_TYPE_GRASS, {{1, 0}, {1, 0}, {1, 0}, {1, 0}, {2, 0}, {0, 0}}},
 	{BLOCK_TYPE_COBBLE, {{3, 0}, {3, 0}, {3, 0}, {3, 0}, {3, 0}, {3, 0}}}
 };
 
-static struct mesh *block_mesh;
-
-
-// TODO: Change incorrect variable types. We use floats where they don't make sense.
-
-void block_build_shared_mesh(void)
-{
-/*	// HACK: Figure out a better way of handling this!
-	if (block_mesh) {
-		LOG("Shared block mesh already created. Creating again...");
-		free(block_mesh);
-	}
-	block_mesh = mesh_create(cube_vertices, sizeof(cube_vertices),
-		cube_elements, sizeof(cube_elements));
-	if (!block_mesh) {
-		LOG("Failed to create block mesh.");
-		block_mesh = NULL;
-		return;
-	}
-	mesh_assign_attr(block_mesh, "pos_attr", 3);
-	mesh_assign_attr(block_mesh, "tex_coord_attr", 2);
-	mesh_process_attr_layout(block_mesh);
-	texture_build(&texture_atlas, "assets\\sprites.jpg");*/
-  }
-
-void block_init(struct block *block, int x, int y, int z, enum block_type type)
+void
+block_init(struct block *block, int x, int y, int z, enum block_type type)
 {
 	if (!block) {
 		return;
@@ -63,46 +35,13 @@ void block_init(struct block *block, int x, int y, int z, enum block_type type)
 	block->x = x;
 	block->y = y;
 	block->z = z;
-	block->visible_faces = BLOCK_FACE_NONE;
 	block->type = type;
 }
 
-void block_draw(struct block *block, unsigned int shd)
+void
+block_get_sprite_offset(enum block_type type, enum block_side side,
+		int *off_x, int *off_y)
 {
-	// HACK: Do we need these variables? Can't we use struct fields?
-	vec2 offset;
-	vec2 size;
-	vec3 pos;
-
-	if (!block) {
-		return;
-	}
-	//texture_bind(&texture_atlas);
-	// HACK: This is unreadable and hacky.
-	size[0] = DEFAULT_TEXTURE_SIZE_PX;
-	size[1] = DEFAULT_TEXTURE_SIZE_PX;
-	pos[0] = (float)(block->x);
-	pos[1] = (float)(block->y);
-	pos[2] = (float)(block->z);
-	for (int i = 0; i < 6; i++) {
-       	 	if (!(block->visible_faces & (1 << i))) {
-         	   continue;
-        	}
-		// HACK: Find out how to avoid these ugly casts.
-		offset[0] = (float)(texture_data[block->type].face[i].offset_x);
-		offset[1] = (float)(texture_data[block->type].face[i].offset_y);
-		// HACK: Pass in shader or keep local shader variable?
-		shader_set_uniform(shd, "offset", offset,
-			SHADER_UNIFORM_TYPE_VEC2);
-		shader_set_uniform(shd, "size", size,
-			SHADER_UNIFORM_TYPE_VEC2);
-		shader_set_uniform(shd, "block_pos", pos,
-			SHADER_UNIFORM_TYPE_VEC3);
-		mesh_draw(block_mesh, 6, i * 6); // bad name for elem count arg. should be NUM_ELEM_PER_FACE etc.
-	}
-}
-
-void block_set_visible_face(struct block *block, int flag)
-{
-	block->visible_faces = flag;
+	*off_x = texture_data[type].side[side].offset_x;
+	*off_y = texture_data[type].side[side].offset_y;
 }
