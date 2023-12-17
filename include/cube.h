72395bbe348f1d25642ac8f35fdabbb63b2ac393
not-kaz <kazislav@protonmail.com>
2023-12-17 16:05:49 +0100

    Changed cube vertices so they are CCW for proper culling.

--- include/cube.h
+++ include/cube.h
@@ -5,60 +5,49 @@
 #define CUBE_NUM_VERTS_PER_FACE 4
 #define CUBE_NUM_COMPS_PER_FACE (CUBE_NUM_COMPS_PER_VERT * CUBE_NUM_VERTS_PER_FACE)
 
-enum cube_face {
-	CUBE_FACE_FRONT = 0,
-	CUBE_FACE_BACK,
-	CUBE_FACE_LEFT,
-	CUBE_FACE_RIGHT,
-	CUBE_FACE_TOP,
-	CUBE_FACE_BOTTOM,
-	NUM_CUBE_FACES
-};
-
-// TODO: Fix these incorrect vertices, not correctly assigned.
 float cube_vertices[] = {
 	// front
-	-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-	0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-	0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-	-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+	-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+	-0.5f, -0.5f, 0.5f, 1.0f, 1.0f,
+	0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
 	// back
-	0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-	0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
-	-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-	//left
-	-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+	0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
 	-0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
-	-0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+	0.5f, 0.5f, -0.5f, 0.0f, 0.0f,
+	//left
+	-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+	-0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
 	-0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
 	//right
-	0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-	0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-	0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-	0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
+	0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+	0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+	0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
 	//top
-	-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-	0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-	0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-	-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+	0.5f, 0.5f, -0.5f, 0.0f, 0.0f,
+	-0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+	-0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+	0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
 	//bottom
 	0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-	0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+	-0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
 	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-	-0.5f, -0.5f, 0.5f, 0.0f, 1.0f
+	0.5f, -0.5f, -0.5f, 1.0f, 0.0f
 };
 
-unsigned int cube_elements[] = {
+unsigned int cube_indices[] = {
 	// front
 	0, 1, 2,
 	2, 3, 0,
 	// back
-	6, 5, 4,
-	4, 7, 6,
+	4, 5, 6,
+	6, 7, 4,
 	// left
-	10, 9, 8,
-	8, 11, 10,
+	8, 9, 10,
+	10, 11, 8,
 	// right
 	12, 13, 14,
 	14, 15, 12,
@@ -66,8 +55,8 @@ unsigned int cube_elements[] = {
 	16, 17, 18,
 	18, 19, 16,
 	// bottom
-	22, 21, 20,
-	20, 23, 22
+	20, 21, 22,
+	22, 23, 20
 };
 
 #endif
